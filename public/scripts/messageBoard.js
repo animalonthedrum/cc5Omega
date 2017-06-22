05b22239c6803ac0b4022d030325a32179493899
Brett Miller <miller.brett.andrew@gmail.com>
2017-06-22 10:04:11 -0500

    login work

--- public/scripts/messageBoard.js
+++ public/scripts/messageBoard.js
@@ -13,7 +13,14 @@ myApp.controller('MessageBoardController', function(MessageBoardService) {
 
   vm.logIn = function() {
     console.log('in controller logIn');
-    MessageBoardService.sendLogIn();
+    var creds = {
+      username: vm.usernameLogin,
+      password: vm.passwordLogin
+    };
+    MessageBoardService.sendLogIn(creds).then(function() {
+      vm.usernameLogin = '';
+      vm.passwordLogin = '';
+    });
   };
 
   vm.logOut = function() {
